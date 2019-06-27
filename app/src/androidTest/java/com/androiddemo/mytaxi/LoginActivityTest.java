26822aa37b17a8d1ba4849265d0441dbe7b144d1
Vinothini <vinothini.rajendran@huf-group.com>
2019-06-27 17:29:44 +0200

    First Commit

---
+++ app/src/androidTest/java/com/androiddemo/mytaxi/LoginActivityTest.java
@@ -0,0 +1,78 @@
+import android.content.res.Resources;
+
+import com.androiddemo.mytaxi.LoginActivity;
+import com.androiddemo.mytaxi.R;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static android.support.test.espresso.matcher.ViewMatchers.withHint;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.AllOf.allOf;
+
+public class LoginActivityTest {
+
+    @Rule
+    public ActivityTestRule mActivityRule = new ActivityTestRule(LoginActivity.class);
+    private Resources resources;
+
+    @Before
+    public void init() {
+        resources = mActivityRule.getActivity().getResources();
+    }
+
+    @Test
+    public void testSuccessfulLOGIN() {
+
+        //Find a view with id user_name and type "Chandler" on that view.
+        onView(withId( R.id.user_name)).perform(typeText("Chandler"));
+
+        //Find a view with has hint Password and type "sarcasm" as password.
+        onView(withHint("Password")).perform(typeText("sarcasm"));
+
+        //Close the keyword, otherwise LoginButton won't be visible to Espresso and it will throw an
+        // exception.
+        closeSoftKeyboard();
+
+        //Find the LOGIN button and perform click().
+        onView(allOf(withText("LOGIN"), isDescendantOfA(withId(R.id.linear_layout))))
+                .perform(click());
+
+        //Assert that the textView of LOGIN TextView has been changed to Success.
+        String loginTxt = resources.getString(R.string.login_success);
+        onView(allOf(withId(R.id.login_progress))).check
+                (matches(withText(loginTxt)));
+    }
+
+    @Test
+    public void testFailedLOGIN() {
+
+        //Find a view with id user_name and type "Ross" on that view.
+        onView(withId(R.id.user_name)).perform(typeText("Ross"));
+
+        //Find a view with has hint Password and type "marriage" as password.
+        onView(withHint("Password")).perform(typeText("marriage"));
+
+        //Close the keyword, otherwise LoginButton won't be visible to Espresso and it will throw an
+        // exception.
+        closeSoftKeyboard();
+
+        //Find the LOGIN button and perform click().
+        onView(allOf(withText("LOGIN"), isDescendantOfA(withId(R.id.linear_layout))))
+                .perform(click());
+
+        //Assert that the textView of LOGIN TextView has been changed to Success.
+        String loginTxt = resources.getString(R.string.login_failure);
+        onView(allOf(withId(R.id.login_progress))).check
+                (matches(withText(loginTxt)));
+    }
+
+}
